862d6dec02a09dafbe3b3030c44e1e06b4fbd490
kgv <kgv@users.noreply.github.com>
2026-01-27 19:53:12 +0300

    6

---
+++ src/_lib.rs
@@ -0,0 +1,23 @@
+use wasm_bindgen::prelude::*;
+
+// Define a function that will be exported to JavaScript
+#[wasm_bindgen]
+pub fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+// You can also import functions from JavaScript into Rust
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet_and_log(name: &str) {
+    let greeting = greet(name);
+    alert(&greeting); // Call JS alert from Rust
+    log(&greeting); // Call JS console.log from Rust
+}
